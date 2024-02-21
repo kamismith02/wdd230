eb81086e48d77299a9dfb7ecaf73b47da9445a4a
kamismith02 <kamilou0709@gmail.com>
2024-02-20 18:52:29 -0700

    lazy loading course preparation

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,3 +1,2 @@
-// getdates.js
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
