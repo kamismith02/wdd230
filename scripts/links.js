b9ccf555fff1152e9a00f13272b9a7cc2d8b2e54
kamismith02 <kamilou0709@gmail.com>
2024-03-06 19:00:08 -0700

    week 09 json menu

---
+++ scripts/links.js
@@ -0,0 +1,25 @@
+const baseURL = "https://kamismith02.github.io/wdd230/";
+const linksURL = `${baseURL}data/links.json`;
+
+async function getLinks() {
+    try {
+        const response = await fetch(linksURL);
+        const data = await response.json();
+        displayLinks(data);
+    } catch (error) {
+        console.error('Error fetching links:', error);
+    }
+}
+
+function displayLinks(weeks) {
+    const listContainer = document.querySelector('.learning-activities ul');
+
+    weeks.forEach(week => {
+        const listItem = document.createElement('li');
+        const lessonNumber = week.lesson.padStart(2, '0');
+        listItem.innerHTML = `<a href="${baseURL}${week.links[0].url}">${lessonNumber}: ${week.links[0].title}</a>`;
+        listContainer.appendChild(listItem);
+    });
+}
+
+getLinks();
