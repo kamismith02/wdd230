799e48848b28dd8ffeef3ac86d504f1556c6eeba
kamismith02 <kamilou0709@gmail.com>
2024-03-11 21:02:09 -0600

    update src for icon

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,24 +1,21 @@
 const apiKey = '74a0ec99bed0ffa4e9a88ae81d66c579';
 
-// City and country code (you can change these)
 const city = 'Orem';
 const countryCode = 'US';
 
 const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}&units=imperial`;
 
-// Fetch weather data from OpenWeatherMap API
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        // Extract required weather information
         const temperature = data.main.temp;
         const description = capitalizeEachWord(data.weather[0].description);
         const iconCode = data.weather[0].icon;
+        const iconsrc = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        // Construct the weather details string
         const weatherDetails = `${temperature}°F - ${description}`;
+        document.getElementById('weather-icon').src = iconsrc;
 
-        // Update DOM elements with weather information
         document.getElementById('weather-details').textContent = weatherDetails;
         document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${iconCode}.png`;
     })
